85e2f60989815bce489776e7841ab9b866cf6c8c
Yozha <130782610+RealYozha@users.noreply.github.com>
2024-09-13 18:38:45 +0300

    ok maybe this will not make this worse

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
-import fstream
-from shutil import rmtree
+import requests
 from pathlib import Path
+import shutil
 
 
-IMAGES_DIRECTORY = "./images"
+def get_ext(filename: str) -> str:
+    extension = Path(filename).suffix
+    return extension
 
 
-def download_dog_picture(number):
-    url = "https://random.dog/woof?filter=mp4,webm"
-    filename = f"random_dog_{number}"
-    path = f"{IMAGES_DIRECTORY}/{filename}"
-    Path(path).mkdir(exist_ok=True)
-    fstream.download_image(url,
-                           path)
+def download_image(url: str, path: str, name_ext: str) -> None:
+    image = requests.get(url=url)
+    image.raise_for_status()
+    with open(path / name_ext, "wb") as file:
+        file.write(image.content)
 
 
-def main():
-    if Path(IMAGES_DIRECTORY).exists():
-        rmtree(IMAGES_DIRECTORY)
-    for i in range(50):
-        download_dog_picture(i)
+def get_dog() -> str:
+    response = requests.get(url="https://random.dog/woof")
+    response.raise_for_status()
+    return response.text
 
 
-if __name__ == "__main__":
+def main() -> None:
+    shutil.rmtree("./assets/pictures")
+    pictures_folder = Path("./assets/pictures")
+    pictures_folder.mkdir(parents=True, exist_ok=False)
+    for i in range(10):
+        file_name: str = get_dog()
+        file_ext: str = get_ext(file_name)
+        if file_ext not in [".mp4", ".webm"]:
+            file_link: str = f"https://random.dog/{file_name}"
+            download_image(file_link,
+                           pictures_folder,
+                           f"dog_{i}{file_ext}")
+
+
+if __name__ == '__main__':
     main()
